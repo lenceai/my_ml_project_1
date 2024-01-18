5e048e5e31c80ececfb74b3b6840e8c7c03c6d79
lenceai <data@lence.ai>
2024-01-17 19:54:03 -0800

    version_0.3

---
+++ file.py
@@ -0,0 +1,12 @@
+total 64
+drwxr-xr-x 3 rlence rlence  4096 Jan 17 19:53 ./
+drwxr-xr-x 3 rlence rlence  4096 Jan 17 18:29 ../
+drwxr-xr-x 8 rlence rlence  4096 Jan 17 19:52 .git/
+-rw-r--r-- 1 rlence rlence   514 Jan 17 19:51 Gas_price_Example.py
+-rw-r--r-- 1 rlence rlence     0 Jan 17 19:53 file.py
+-rw-r--r-- 1 rlence rlence 28657 Jan 17 18:56 hw_0.1.ipynb
+-rw-r--r-- 1 rlence rlence   306 Jan 17 18:56 hw_pff2.py
+-rw-r--r-- 1 rlence rlence   532 Jan 17 18:39 hw_pff3.py
+-rw-r--r-- 1 rlence rlence   394 Jan 17 18:39 hw_pff5.py
+-rw-r--r-- 1 rlence rlence   408 Jan 17 18:39 hw_pff9.py
+-rw-r--r-- 1 rlence rlence   372 Jan 17 18:38 links.py
